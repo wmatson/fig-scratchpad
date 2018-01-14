51a29dc53dcd6e982f1e1a0bed274283c07566c2
Wesley Matson <wmatson@live.com>
2018-01-14 11:09:44 -0700

    Minor refactors, pulling calculations into their own functions

--- src/fig_scratchpad/core.cljs
+++ src/fig_scratchpad/core.cljs
@@ -16,6 +16,13 @@
 
 (defn feet->m [feet]
   (* 0.3048 feet))
+
+(defn bmi [height-meters weight-kg]
+  (/ weight-kg
+     (* height-meters height-meters)))
+
+(defn target-hr [max-hr resting-hr intensity]
+  (+ resting-hr (* intensity (- max-hr resting-hr))))
   
 (def lbs-cursor (rum/cursor app-state :lbs))
 (def height-cursor (rum/cursor app-state :feet))
@@ -40,25 +47,25 @@
        :value (rum/react cursor)
        :on-change (on-change-swap cursor)}]]))
 
-(rum/defc bmi-calc < rum/reactive []
+(rum/defc bmi-display < rum/reactive []
   [:div
    "BMI: "
    (let [height (feet->m (rum/react height-cursor))
-         bmi (/ (lbs->kg (rum/react lbs-cursor))
-                (* height height))]
+         weight (lbs->kg (rum/react lbs-cursor))
+         bmi (bmi height weight)]
      [:span (str bmi)])])
 
-(rum/defc max-heart-rate-calc < rum/reactive []
+(rum/defc max-heart-rate-display < rum/reactive []
   [:div
    "Max Heart Rate:"
    [:span (str (- 220 (rum/react age-cursor)))]])
 
-(rum/defc target-heart-rate-calc < rum/reactive [intensity]
+(rum/defc target-heart-rate-display < rum/reactive [intensity]
   [:div
    (str (* 100 intensity) "% Target Heart Rate:")
    (let [max-hr (- 220 (rum/react age-cursor))
          resting-hr (* 1 (rum/react resting-hr-cursor))]
-     [:span (str (+ resting-hr (* intensity (- max-hr resting-hr))))])])
+     [:span (str (target-hr max-hr resting-hr intensity))])])
 
 (rum/defc hello-world []
   [:div.container-fluid
@@ -68,11 +75,11 @@
      (input "Height (feet)" height-cursor)
      (input "Age (years)" age-cursor)
      (input "Resting Heart Rate (bpm)" resting-hr-cursor)]]
-   (bmi-calc)
-   (max-heart-rate-calc)
-   (target-heart-rate-calc 1)
-   (target-heart-rate-calc 0.70)
-   (target-heart-rate-calc 0.40)])
+   (bmi-display)
+   (max-heart-rate-display)
+   (target-heart-rate-display 1)
+   (target-heart-rate-display 0.70)
+   (target-heart-rate-display 0.40)])
 
 (rum/mount (hello-world)
            (. js/document (getElementById "app")))
